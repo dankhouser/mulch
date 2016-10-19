2a4ab338494a3f3f23133480a286e1fc657588cc
Shea Fankhouser <sheafankhouser@gmail.com>
2016-10-19 10:20:58 -0500

    Update mulchCalc.js

--- javascripts/mulchCalc.js
+++ javascripts/mulchCalc.js
@@ -2,7 +2,7 @@ function calculateArea() {
   var length = document.getElementById("length").value;
   var width = document.getElementById("width").value;
   var depth = document.getElementById("depth").value;
-  var tons = ((length * width * (depth/12))/27)*2.36;
+  var tons = ((length * width * (depth/12))/27)*2.96;
   var tons2 = tons.toFixed(2);
   document.getElementById("total").innerHTML = tons2
 }
